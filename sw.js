024cab1b5db70c358e5cde6ccc429cfc8b6471b2
mtm951 <mike.markota@gmail.com>
2026-09-16 14:55:16 -0400

    Refresh offline shell for v4.9.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='n594zs-v4-9-1-shell-1';
+const CACHE='n594zs-v4-9-2-shell-1';
 const SHELL=['./','./index.html','./styles.css','./enhancements.css','./workflow.css','./ops.css','./data-tools.css','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
