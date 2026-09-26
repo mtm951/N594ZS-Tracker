f33f01888fe3e20a7f3b1d182a1609d89eaf7a0f
mtm951 <mike.markota@gmail.com>
2026-09-26 16:16:44 -0400

    Bind visible footer to actual running APP_VERSION; prepare v5.19.36

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='n594zs-v5-19-35-shell';
+const CACHE='n594zs-v5-19-36-shell';
 const SHELL=['./','./index.html','./styles.css','./enhancements.css','./workflow.css','./ops.css','./data-tools.css','./aircraft-ops.css','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
